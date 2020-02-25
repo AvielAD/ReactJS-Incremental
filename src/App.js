be899a75b307047160873951438a93028d1d4ff3
Aviel <aviel.aldama@gmail.com>
2020-02-24 20:33:18 -0600

    teststateless

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import React, {Component} from 'react';
 import './index.css';
-
+import Controlado from './Controlado';
 class App extends Component{
 
   constructor(props){
     super(props);
     this.state = { 
-      clicks: props.clicks,
-      titulo: props.titulo,
     };
   }
 
   render(){
-    
-    const {titulo} = this.props;
-    const {clicks} = this.state;
 
     return (
       <React.Fragment>
-        <h1>{titulo}</h1>
-        <span>{clicks}</span>
-        <br/>
-        <button onClick={ ()=>{
-          this.setState({clicks: clicks + 1});
-        }}>Click me</button>
+          <Controlado></Controlado>
+
 
       </React.Fragment>
     );
